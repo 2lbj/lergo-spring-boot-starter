757f0abcd08d4525e8d6989d1caafe00af1997b5
2lbj <nerv.2lbj@gmail.com>
2023-07-13 10:41:16 +0800

    load to JDK 8

--- src/main/java/com/lergo/framework/config/SwaggerConfig.java
+++ src/main/java/com/lergo/framework/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 import javax.annotation.Resource;
 
-@EnableConfigurationProperties(value = {SwaggerProperties.class})
+@EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerConfig {
 
     @Resource
