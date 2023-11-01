d960d595e24e639be22978d82e69e9940236166e
2lbj <nerv.2lbj@gmail.com>
2023-11-01 16:15:52 +0800

    fix config path

--- src/main/java/com/lergo/framework/LergoBootStarter.java
+++ src/main/java/com/lergo/framework/LergoBootStarter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Arrays;
 
@@ -12,7 +13,7 @@ public class LergoBootStarter extends AbstractBootStarter {
 	private static final Logger log = LoggerFactory.getLogger(LergoBootStarter.class);
 
 	public LergoBootStarter() {
-		System.setProperty("pagehelper.banner", "false");
+		//System.setProperty("pagehelper.banner", "false");
 		log.info("{}-{} ({})",
 				System.getProperties().getProperty("os.name"),
 				System.getProperties().getProperty("os.version"),
@@ -21,8 +22,10 @@ public class LergoBootStarter extends AbstractBootStarter {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(LergoBootStarter.class, args);
-        System.out.println("Spring BOOT Startup: $" + Arrays.toString(args));
+		ConfigurableApplicationContext run = SpringApplication.run(LergoBootStarter.class, args);
+		System.out.println("OK --- Spring BOOT Running: $" + Arrays.toString(args));
+		System.out.printf("Swagger-UI: http://localhost:%s%n",
+				run.getEnvironment().getProperty("server.port"));
 	}
 
 }
