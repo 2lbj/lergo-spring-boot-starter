e2af267f6f81d6d4630eeaa764286bd0657e0365
2lbj <nerv.2lbj@gmail.com>
2024-02-26 00:46:29 +0800

    fix

--- src/main/java/com/lergo/framework/config/LergoConfig.java
+++ src/main/java/com/lergo/framework/config/LergoConfig.java
@@ -4,33 +4,13 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Data
 @Component
 @ConfigurationProperties(prefix = "lergo")
 public class LergoConfig {
 
     /**
-     * 死道友不死贫道
+     * LerGo版本
      */
-    List<ChannelTemplate> channel;
-
-    /**
-     * 键值对
-     */
-    @Data
-    public static class ChannelTemplate {
-
-        /**
-         * 代码
-         */
-        Integer code;
-
-        /**
-         * 名称
-         */
-        String name;
-    }
-
+    String version;
 }
