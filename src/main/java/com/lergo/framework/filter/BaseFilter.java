a44d61a059e46c443463c90e6d299c3f759619a1
2lbj <nerv.2lbj@gmail.com>
2024-03-06 02:13:42 +0800

    fix

--- src/main/java/com/lergo/framework/filter/BaseFilter.java
+++ src/main/java/com/lergo/framework/filter/BaseFilter.java
@@ -10,8 +10,8 @@ public class BaseFilter {
     ObjectMapper objectMapper = new ObjectMapper();
     boolean writeList(ServerHttpRequest req) {
         return req.getPath().value().equals("/") ||
-                req.getPath().value().startsWith("/actuator") ||
-                req.getPath().value().startsWith("/v3/api-docs") ||
+                req.getPath().value().endsWith("/actuator") ||
+                req.getPath().value().endsWith("/v3/api-docs") ||
                 req.getPath().value().equals("/swagger-ui.html") ||
                 req.getPath().value().startsWith("/webjars") ||
                 req.getPath().value().contains(".html") ||
