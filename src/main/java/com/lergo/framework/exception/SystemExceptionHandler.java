03ba3f73114b1b5ac55c3ee78c1a0f66061a0323
2lbj <nerv.2lbj@gmail.com>
2024-03-12 02:02:20 +0800

    fix ExceptionHandler order

--- src/main/java/com/lergo/framework/exception/SystemExceptionHandler.java
+++ src/main/java/com/lergo/framework/exception/SystemExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.lergo.framework.exception;
 
 import com.lergo.framework.entity.CommonResult;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.map.HashedMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.server.UnsupportedMediaTypeStatusException;
 
 import java.util.Arrays;
@@ -36,6 +40,51 @@ public class SystemExceptionHandler {
         return e.getMessage();
     }
 
+    ///////////////////////////////////////////////////////////////////////////
+
+    /*
+     * 处理单个参数校验失败抛出的异常
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public HashedMap<Path, String> constraintViolationExceptionHandler(ConstraintViolationException e) {
+        return e.getConstraintViolations().parallelStream()
+                .peek(v -> log.warn("{} {} <-- [{}]", v.getPropertyPath(),
+                        v.getMessage(), v.getInvalidValue()))
+                .collect(HashedMap::new, (m, v) ->
+                                m.put(v.getPropertyPath(), v.getMessage()),
+                        HashedMap::putAll);
+    }
+
+    /*
+     * 处理请求体调用接口校验失败抛出的异常
+     */
+    @ExceptionHandler(WebExchangeBindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public HashedMap<String, String> methodArgumentNotValidExceptionHandler(WebExchangeBindException e) {
+        return e.getBindingResult().getFieldErrors().parallelStream()
+                .peek(v -> log.warn("{{}.{}} {} <-- [{}]", v.getObjectName(), v.getField(),
+                        v.getDefaultMessage(), v.getRejectedValue()))
+                .collect(HashedMap::new, (m, v) ->
+                                m.put(v.getField(), v.getDefaultMessage()),
+                        HashedMap::putAll);
+    }
+
+
+//    @ExceptionHandler(value = {BindException.class, ValidationException.class, MethodArgumentNotValidException.class})
+//    //处理 form data方式调用接口校验失败抛出的异常
+//    @ExceptionHandler(org.springframework.validation.BindException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public HashedMap<String, String> bindExceptionHandler(org.springframework.validation.BindException e) {
+//        return e.getBindingResult().getFieldErrors().parallelStream()
+//                .peek(v -> log.warn("{{}.{}} {} <-- [{}]", v.getObjectName(), v.getField(),
+//                        v.getDefaultMessage(), v.getRejectedValue()))
+//                .collect(HashedMap::new, (m, v) ->
+//                                m.put(v.getField(), v.getDefaultMessage()),
+//                        HashedMap::putAll);
+//    }
+
+    ///////////////////////////////////////////////////////////////////////////
 
     /*
      * 自定义系统异常
