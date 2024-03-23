4e035881fff83c1233f07a64eaad9750d5ee800e
2lbj <nerv.2lbj@gmail.com>
2024-03-23 19:46:27 +0800

    fix actuator

--- src/main/java/com/lergo/framework/filter/BaseFilter.java
+++ src/main/java/com/lergo/framework/filter/BaseFilter.java
@@ -11,7 +11,7 @@ public class BaseFilter {
     boolean writeList(ServerHttpRequest req) {
         return req.getPath().value().equals("/") ||
                 req.getPath().value().startsWith("/actuator") ||
-                req.getPath().value().equals("/v3/api-docs") ||
+                req.getPath().value().startsWith("/v3/api-docs") ||
                 req.getPath().value().equals("/swagger-ui.html") ||
                 req.getPath().value().startsWith("/webjars") ||
                 req.getPath().value().contains(".html") ||
